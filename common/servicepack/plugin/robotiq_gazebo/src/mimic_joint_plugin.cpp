8775560f0d4ae0b45212a852e3211cea0e34096a
ros.robotics <ros.robotics@doosan.com>
2020-06-24 20:24:16 +0900

    update robotiq_2f_plugin to melodic version

--- common/servicepack/plugin/robotiq_gazebo/src/mimic_joint_plugin.cpp
+++ common/servicepack/plugin/robotiq_gazebo/src/mimic_joint_plugin.cpp
@@ -47,7 +47,7 @@ MimicJointPlugin::MimicJointPlugin()
 
 MimicJointPlugin::~MimicJointPlugin()
 {
-  event::Events::DisconnectWorldUpdateBegin(this->updateConnection);
+  this->updateConnection.reset();
 
   kill_sim = true;
 }
@@ -167,20 +167,20 @@ void MimicJointPlugin::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf )
 
 void MimicJointPlugin::UpdateChild()
 {
-  static ros::Duration period(world_->GetPhysicsEngine()->GetMaxStepSize());
+  static ros::Duration period(world_->Physics()->GetMaxStepSize());
 
   // Set mimic joint's angle based on joint's angle
-  double angle = joint_->GetAngle(0).Radian()*multiplier_+offset_;
+  double angle = joint_->Position(0)*multiplier_+offset_;
   
-  if(abs(angle-mimic_joint_->GetAngle(0).Radian())>=sensitiveness_)
+  if(abs(angle-mimic_joint_->Position(0))>=sensitiveness_)
   {
     if(has_pid_)
     {
-      double a = mimic_joint_->GetAngle(0).Radian();
+      double a = mimic_joint_->Position(0);
       if(a!=a)
         a = angle;
       double error = angle-a;
-      double effort = gazebo::math::clamp(pid_.computeCommand(error, period), -max_effort_, max_effort_);
+      double effort = ignition::math::clamp(pid_.computeCommand(error, period), -max_effort_, max_effort_);
     }
     else
     {
